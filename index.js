1b18419c862a8b3314919a8c3dfddaf3e24cd786
Robert Gabriel <anyrobert@outlook.com.br>
2023-02-24 10:49:42 -0300

    fix: fix mobile view port issues

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const toggleNav = () => {
   document.body.dataset.nav =
     document.body.dataset.nav === "true" ? "false" : "true";
 };
+
+let viewHeight = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", `${viewHeight}px`);
+
+window.addEventListener("resize", () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
